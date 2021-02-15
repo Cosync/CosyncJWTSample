5b096256f92aed25febb547cbfc0a586b1561218
Tola Voeung <tolacambo@gmail.com>
2021-02-15 15:40:11 +0700

    update react native JWT

---
+++ ReactNative/CosyncJWT/managers/CosyncJWTManager.js
@@ -0,0 +1,234 @@
+//
+//  RealmManager.js
+//  CosyncJWT
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//  Created by Tola Voeung.
+//  Copyright © 2020 cosync. All rights reserved.
+//
+import Configure from '../config/Config' 
+import Request from '../components/Request'; 
+import md5 from 'md5';
+
+export const signup = (firstName, lastName, handle, userPassword) => {
+    return new Promise((resolve, reject) => { 
+
+        let metaData = {
+            name: {
+                first: firstName,
+                last: lastName
+            },
+            email: handle
+        };
+
+
+        let dataToSend = {
+            handle: handle,
+            password: md5(userPassword),
+            metaData : JSON.stringify(metaData)
+        }; 
+        
+        Request(`${Configure.CosyncApp.apiURL}/api/appuser/signup`, dataToSend, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8',
+                'app-token': Configure.CosyncApp.appToken
+            },
+             
+        }, (progressEvent) => { 
+            
+           
+        }).then((res) => { 
+            
+            try {
+                
+                let respone = JSON.parse(res._response); 
+                if(res.status == 200 ) resolve(respone)
+                else reject(respone)
+
+            } catch (error) {
+                reject(res._response)
+            }
+            
+
+        }, (err) => reject(err) )
+        
+    })
+  }
+  
+  export const completeSignup = (handle , code) => {
+    return new Promise((resolve, reject) => { 
+        let dataToSend = {
+            handle: handle,
+            code: code 
+        }; 
+        
+        Request(`${Configure.CosyncApp.apiURL}/api/appuser/completeSignup`, dataToSend, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8',
+                'app-token': Configure.CosyncApp.appToken
+            },
+             
+        }, (progressEvent) => { 
+            
+           
+        }).then((res) => { 
+            try {
+
+                let respone = JSON.parse(res._response); 
+                if(res.status == 200 ) resolve(respone)
+                else reject(respone)
+
+            } catch (error) {
+                reject(res._response)
+            }
+            
+
+        }, (err) => reject(err) )
+    })
+  }
+  
+    export const login = (handle, userPassword) => {
+        return new Promise((resolve, reject) => { 
+    
+            let dataToSend = {
+                handle: handle,
+                password: md5(userPassword)
+            }; 
+            
+            Request(`${Configure.CosyncApp.apiURL}/api/appuser/login`, dataToSend, {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8',
+                    'app-token': Configure.CosyncApp.appToken
+                },
+                
+            }, (progressEvent) => { 
+                
+            
+            }).then((res) => { 
+                try {
+                    
+                    let respone = JSON.parse(res._response); 
+                    if(res.status == 200 ) resolve(respone)
+                    else reject(respone)
+
+                } catch (error) {
+                    reject(res._response)
+                }
+                
+
+            }, (err) => reject(err) )
+        
+        })
+    }
+
+
+    export const forgotPassword = (handle) => {
+        return new Promise((resolve, reject) => { 
+    
+            let dataToSend = {
+                handle: handle 
+            }; 
+            
+            Request(`${Configure.CosyncApp.apiURL}/api/appuser/forgotPassword`, dataToSend, {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8',
+                    'app-token': Configure.CosyncApp.appToken
+                },
+                
+            }, (progressEvent) => { 
+                
+            
+            }).then((res) => { 
+                try {
+                    
+                    let respone = JSON.parse(res._response); 
+                    if(res.status == 200 ) resolve(respone)
+                    else reject(respone)
+
+                } catch (error) {
+                    reject(res._response)
+                }
+                
+
+            }, (err) => reject(err) )
+        
+        })
+    }
+
+    export const fetchData = (endpoint, method, data) => {
+        return new Promise((resolve, reject) => {  
+            let option = {
+                method: method || 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'app-token': Configure.CosyncApp.appToken
+                },
+            };
+
+            if(data) option.body = JSON.stringify(data);
+
+            fetch(`${Configure.CosyncApp.apiURL}${endpoint}`, option)
+            .then((response) => response.json())
+            .then((json) => resolve(json))
+            .catch((error) => reject(error)); 
+        })
+    }
+
+    export const getCosyncApplicationData = () => {
+        return new Promise((resolve, reject) => {  
+
+            let dataToSend = {
+                handle: 'handle' 
+            }; 
+
+            Request(`${Configure.CosyncApp.apiURL}/api/appuser/getApplication`,dataToSend, {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8',
+                    'app-token': Configure.CosyncApp.appToken
+                },
+                
+            }, (progressEvent) => { 
+                
+            
+            }).then((res) => { 
+                try {
+
+                    let respone = JSON.parse(res._response); 
+                    if(res.status == 200 ) resolve(respone)
+                    else reject(respone)
+
+                } catch (error) {
+                    console.log(error);
+                    reject(res._response)
+                }
+                
+
+            }, (err) => {
+                console.log(err);
+                reject(err)
+            })
+        })
+    }
+    
---
+++ ReactNative/CosyncJWT/screens/ProfileScreen.js
@@ -0,0 +1,116 @@
+ 
+//
+//  ProfileScreen.js
+//  CosyncJWT
+//
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+//
+//  Created by Tola Voeung.
+//  Copyright © 2020 cosync. All rights reserved.
+//
+
+import React, { useEffect, useState, useRef } from 'react'; 
+import {
+  StyleSheet, 
+  View,
+  Text,
+ 
+} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import Loader from '../components/Loader'; 
+import Configure from '../config/Config';  
+
+const ProfileScreen = props => { 
+  let [loading, setLoading] = useState(false);
+   
+  const ref_input_pwd = useRef(); 
+
+  global.appId = Configure.Realm.appId; 
+  AsyncStorage.setItem('appId', global.appId);  
+
+ 
+  useEffect(() => {
+    CosyncJWT.getCosyncApplicationData().then(result => { 
+      global.appData = result;
+    });
+  }, []);
+
+
+  return (
+    <View style={styles.mainBody}>
+      <Loader loading={loading} />
+      
+       <Text>Profile Page</Text>
+    </View>
+  );
+};
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  mainBody: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  SectionStyle: {
+    flexDirection: 'row',
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: '#4638ab',
+    borderWidth: 0,
+    color: '#FFFFFF',
+    borderColor: '#7DE24E',
+    height: 40,
+    alignItems: 'center',
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonTextStyle: {
+    color: 'white',
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: '#4638ab',
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: '#4638ab',
+  },
+  registerTextStyle: {
+    color: '#4638ab',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
+});
